bca787778299c070c4baf0ee4569029d0b4f76be
diegoluan <diegoluan@weg.net>
2023-03-29 21:14:11 -0300

    Created ImageExtractor

---
+++ src/ImageExtractorImdb.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class ImageExtractorImdb implements ImageExtractor {
+
+    @Override
+    public List<Image> extractImages(String json) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'extractImages'");
+    }
+    
+}
---
+++ src/ImageExtractorNasa.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ImageExtractorNasa implements ImageExtractor {
+    
+    @Override
+    public List<Image> extractImages(String json){
+        
+        return null;
+    }
+}
